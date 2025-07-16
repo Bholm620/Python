61d695762efe0a671df4c4b321c91186e2118331
Bholm620 <BHOLM886@GMAIL.COM>
2025-07-15 20:31:37 -0600

    Create Inheritance.py

---
+++ Inheritance.py
@@ -0,0 +1,12 @@
+class Instrument:
+    name = 'No Name Provided'
+    style = ''
+    color = ''
+
+class Stringed(Instrument):
+    num_strings = ''
+    meth_struck = ''
+
+class Woodwind(Instrument):
+    reed = ''
+    valves = ''
