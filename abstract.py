b29c50712379541280a1084e023a70cbb313b0c2
Bholm620 <BHOLM886@GMAIL.COM>
2025-07-25 22:53:11 -0600

    Update abstract.py

--- abstract.py
+++ abstract.py
@@ -1,5 +1,5 @@
-
-
+# Abstract assignment
+# Abstract method
 from abc import ABC, abstractmethod
 class music(ABC):
     def blues(self, instrument):
@@ -7,11 +7,11 @@ class music(ABC):
         @abstractmethod
         def style(self, guitar):
             pass
-
+# regular method with child class
 class guitar(music):
     def accompaniment(self, instrument):
         print("The blues also sounds good on the {} because it's sad".format(instrument))
-
+# object creation utilizing both
 obj = guitar()
 obj.blues("guitar")
 obj.accompaniment("trumpet")
